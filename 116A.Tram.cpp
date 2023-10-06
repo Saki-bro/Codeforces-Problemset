815a2aa204a9e6b623349da543b1e4d413592a0e
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2023-10-07 00:09:17 +0600

    Add files via upload

---
+++ 116A.Tram.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s,t;
+    string rev;
+    cin>> s;
+    cin>> t;
+
+    int len = s.length();
+
+    for(int i=len-1; i>=0; i--)
+    {
+
+
+    }
+
+
+
+
+}
