0ed3461d192dc6c6733c091ce8bf4fa8501128ed
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2023-10-08 19:32:12 +0600

    116A.Tram.cpp

--- 116A.Tram.cpp
+++ 116A.Tram.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    string s,t;
-    string rev;
-    cin>> s;
+    int t;
     cin>> t;
-
-    int len = s.length();
-
-    for(int i=len-1; i>=0; i--)
+    int pass_in=0, pass_total=0;
+    while(t--)
     {
+        int a,b;
+        cin>> a>> b;
+        pass_in -= a;
+        pass_in += b;
 
-
+        if(pass_in > pass_total)
+        {
+            pass_total = pass_in;
+        }
     }
-
-
-
-
+    cout<< pass_total<<endl;
 }
