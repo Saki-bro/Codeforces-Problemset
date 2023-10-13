1205dae0c66a22659003fca4d89419ca56aa643a
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2023-10-14 02:17:41 +0600

    Add files via upload

---
+++ 1881A. Don't Try to Count.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+
+
+        long long n, m;
+        cin >> n >> m;
+        string x, s;
+        cin >> x >> s;
+        long long c = 0;
+        for(int i=0; i<10; i++)
+        {
+        if(x.find(s) != -1)
+        {
+            cout<<i<<endl;
+            c++;
+            break;
+        }
+         x+=x;
+        }
+        if (c == 0)
+            {
+                cout << "-1" << endl;
+            }
+
+    }
+
+
+}
