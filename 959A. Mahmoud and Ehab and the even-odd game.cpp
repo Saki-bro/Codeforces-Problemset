edcd58deba98ade542a14b143b6d9abaec6739ca
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2023-12-12 01:28:57 +0600

    Add files via upload

---
+++ 959A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+
+    int n;
+    cin>>n;
+    if(n%2==0)cout<<"Mahmoud";
+    else cout<< "Ehab";
+
+}
+  main()
+{
+    solve();
+    return 0;
+}
+
+
+
+
