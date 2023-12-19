925465b4c7891468dbff02e96d79f790a8b8c7ee
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2023-12-19 15:03:48 +0600

    Add files via upload

---
+++ 1913A. Rating Increase.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+
+    int n;
+    cin>>n;
+    int res=0;int pow=1;
+    while(n>0)
+    {
+        res +=pow*(n%10);
+        n=n/10;
+        if(n!=0 && res>=pow)
+        {
+            if(res>n)
+            {
+                cout<< n << " " << res << endl;
+                return;
+            }
+        }
+        pow *= 10;
+    }
+    cout<< -1 << endl;
+
+}
+  main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
