d044953bc060724fe1c37aa147edcd329a4e6700
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-01-07 21:15:03 +0600

    Add files via upload

---
+++ 1594A. Consecutive Sum Riddle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+
+    int n;
+    cin>>n;
+    cout<< -n+1 << " " << n << endl;
+
+}
+  main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
