8e841ce972c88ddae615f2148937beeb5a40282f
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-01-13 00:29:16 +0600

    Add files via upload

---
+++ 1102A. Integer Sequence Dividing.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+
+    int n;
+    cin>>n;
+    int ans = n*(n+1)/2;
+    cout<< ans%2 << endl;
+
+}
+  main()
+{
+    solve();
+    return 0;
+}
+
+
