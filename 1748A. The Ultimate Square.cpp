23265e0faa2fce452e9bd61f2f4c49d678076dc4
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-01-13 00:29:35 +0600

    Add files via upload

---
+++ 1748A. The Ultimate Square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int ans = ceil ((double)n/2);
+    cout<< ans << endl;
+}
+  main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
