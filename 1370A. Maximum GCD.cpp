bf28b63231f9c312108ba69a7ac61f0d713df80c
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-03-13 05:47:33 +0600

    Add files via upload

---
+++ 1370A. Maximum GCD.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define int long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+    int n;
+    cin>> n;
+    cout<< n/2 << endl;
+
+}
+  main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
