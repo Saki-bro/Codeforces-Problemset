2e6c5170aa6a8b3878ab97a3978fb8e2a6bdb1e8
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-05-25 02:44:00 +0600

    Add files via upload

---
+++ 1952A. Are You a Robot, Again.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define int long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+   cout << "safety" ;
+}
+main()
+{
+    solve();
+    return 0;
+}
+
+
+
