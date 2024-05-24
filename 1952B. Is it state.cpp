d88c5bc3686eb105983bef6122890b12be9f3313
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-05-25 02:43:45 +0600

    Add files via upload

---
+++ 1952B. Is it state.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+#define int long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+    string s;
+    cin>> s;
+
+    if(s.find("it") != -1)
+    {
+        yes;
+    }
+    else
+    {
+        no;
+    }
+
+
+}
+main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+
