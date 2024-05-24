4130519f2301f28fbf57dd95e9f5d96a144270a2
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-05-25 02:33:32 +0600

    Add files via upload

---
+++ 1952D. Are You a Procrastinator.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+     char c;
+     cin>> c;
+     cout<< "yes" << endl;
+}
+main()
+{
+
+        solve();
+    return 0;
+}
+
+
+
