4c181ab57ad51f386b89ddb17c7c149f904e582a
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-06-03 23:10:31 +0600

    Add files via upload

---
+++ 1981A. Turtle and Piggy Are Playing a Game.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+    int l,r;
+    cin>> l >>r;
+    cout<< __lg(r) << endl;
+}
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
