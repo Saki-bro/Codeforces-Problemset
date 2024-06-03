fb1997c6a0760e9d33aca09eafd5a97ed8a1febc
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-06-03 23:10:55 +0600

    Add files via upload

---
+++ 1977A. Little Nikita.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+using namespace std;
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    if(n>=m && (m-n)%2 == 0)
+    {
+        yes;
+    }
+    else
+    {
+        no;
+    }
+}
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
