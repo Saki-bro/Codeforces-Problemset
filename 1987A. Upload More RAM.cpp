e146525a4047c801787805f753a4414004549aa1
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-07-06 17:01:43 +0600

    Add files via upload

---
+++ 1987A. Upload More RAM.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+
+/*---------------Bismillah---------------------*/
+
+void solve()
+{
+    int n,k;cin>>n>>k;
+
+    cout<< ((n-1)*k)+1 << endl;
+}
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+  solve();
+    }
+    return 0;
+}
+
