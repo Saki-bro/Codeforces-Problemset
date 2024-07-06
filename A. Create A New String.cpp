2273181ec8f61de60e40abaa23656d4db3c1c2c9
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2024-07-06 17:02:59 +0600

    Add files via upload

---
+++ A. Create A New String.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<< "YES"<< endl
+#define no cout<< "NO"<< endl
+
+/*---------------Bismillah---------------------*/
+
+void solve()
+{
+    string s,r;
+    cin>> s >> r;
+
+    cout<< s.size()<< " " << r.size()<< endl;
+    cout<< s << " " << r << endl;
+}
+
+int main()
+{
+//    ll t;
+//    cin>>t;
+//    while(t--)
+//    {
+        solve();
+//    }
+    return 0;
+}
+
+
+
+
